9594b66c26f7d7cbfbe258857c1ba31cdab3fb59
Fabrice Matrat <fabricematrat@gmail.com>
2014-08-20 20:51:24 +0200

    new rave wire

--- app/main.js
+++ app/main.js
@@ -2,17 +2,17 @@ define({
 
 	// Load a basic theme. This is just a CSS file, and since a moduleLoader is
 	// configured in run.js, curl knows to load this as CSS.
-	theme: { module: 'theme/basic.css' },
+	//theme: { module: 'theme/basic.css' },
 
 	// Create a simple view by rendering html, replacing some i18n strings
 	// and loading CSS.  Then, insert into the DOM
 	message: {
 		render: {
-			template: { module: 'text!welcome/template.html' },
-			replace: { module: 'i18n!welcome/strings' },
-			css: { module: 'css!welcome/structure.css' }
+			template: { module: 'template.html' }
+			//replace: { module: 'i18n!welcome/strings' },
+			//css: { module: 'css!welcome/structure.css' }
 		},
-		insert: { at: 'dom.first!body' }
+		//insert: { at: 'dom.first!body' }
 	},
 
 	// Wire.js plugins
