caf456ed9cfbf36ad11a75e0699cc939c175ade8
Fabrice Matrat <fabricematrat@gmail.com>
2014-08-20 21:32:28 +0200

    First working version

--- app/main.js
+++ app/main.js
@@ -2,17 +2,17 @@ define({
 
 	// Load a basic theme. This is just a CSS file, and since a moduleLoader is
 	// configured in run.js, curl knows to load this as CSS.
-	//theme: { module: 'theme/basic.css' },
+	theme: { module: 'rave-start-cujo/theme/basic.css' },
 
 	// Create a simple view by rendering html, replacing some i18n strings
 	// and loading CSS.  Then, insert into the DOM
 	message: {
 		render: {
-			template: { module: 'template.html' }
-			//replace: { module: 'i18n!welcome/strings' },
-			//css: { module: 'css!welcome/structure.css' }
+			template: { module: 'rave-start-cujo/welcome/template.html' },
+			replace: { module: 'rave-start-cujo/welcome/strings' },
+			css: { module: 'rave-start-cujo/welcome/structure.css' }
 		},
-		//insert: { at: 'dom.first!body' }
+		insert: { at: 'dom.first!body' }
 	},
 
 	// Wire.js plugins
