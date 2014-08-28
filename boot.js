a7f7437f5f1a1d569f472d252e1b76f6e1a0a93b
Fabrice Matrat <fabricematrat@gmail.com>
2014-08-29 01:45:34 +0200

    Fix for IE

--- boot.js
+++ boot.js
@@ -1,8 +1,8 @@
 (function(d,s){
-	s=(d.head||d.getElementsByTagName('head')[0]).appendChild(d.createElement('script'));
-	s.async=!1;
-	s.src='bower_components/rave/rave.js';
 	h=(d.html||d.getElementsByTagName('html')[0])
 	h.setAttribute('data-debug','');
 	h.setAttribute('data-rave-meta','bower.json');
+	s=(d.head||d.getElementsByTagName('head')[0]).appendChild(d.createElement('script'));
+	s.async=!1;
+	s.src='bower_components/rave/rave.js';
 }(document));
